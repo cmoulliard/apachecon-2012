69f76612858211cdbc29026106bc7132de510b7c
Charles Moulliard <cmoulliard@fusesource.com>
2012-11-05 09:57:39 +0100

    Add CSV & bindy

--- karafee/integration/src/main/java/org/apache/con2012/karafee/integration/MyBean.java
+++ karafee/integration/src/main/java/org/apache/con2012/karafee/integration/MyBean.java
@@ -1,11 +1,41 @@
 package org.apache.con2012.karafee.integration;
 
+import org.apache.camel.Body;
+import org.apache.con2012.karafee.integration.model.ConferenceCsvModel;
+import org.apache.con2012.karafee.model.Conference;
+import org.apache.con2012.karafee.service.ConferenceService;
+
+import javax.inject.Inject;
+
 /**
  * Simple Bean/PoJo class
  */
 public class MyBean {
 
+    @Inject
+    ConferenceService conferenceService;
+
     public void someMethod(String body) {
         System.out.println("Message received : " + body);
+    }
+
+    public void saveConference(@Body ConferenceCsvModel model) {
+
+        // Map Bindy Model with Entity Model
+        Conference entity = new Conference();
+        entity.setId(model.getId());
+        entity.setCreationUser(model.getCreationUser());
+        entity.setDate(model.getDate());
+        entity.setRef(model.getRef());
+        entity.setSummary(model.getSummary());
+        entity.setDetails(model.getDetails());
+        entity.setEmail(model.getEmail());
+        entity.setPhone(model.getPhone());
+        entity.setFamilyName(model.getFamilyName());
+        entity.setGivenName(model.getGivenName());
+
+        // Save conference
+        conferenceService.store(entity);
+
     }
 }
--- karafee/integration/src/main/java/org/apache/con2012/karafee/integration/MyRouteBuilder.java
+++ karafee/integration/src/main/java/org/apache/con2012/karafee/integration/MyRouteBuilder.java
@@ -2,6 +2,9 @@ package org.apache.con2012.karafee.integration;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.ContextName;
+import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.apache.con2012.karafee.integration.model.ConferenceCsvModel;
+import org.apache.con2012.karafee.service.ConferenceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,19 +28,28 @@ public class MyRouteBuilder extends RouteBuilder {
      */
     public void configure() {
 
-        // here is a sample which processes the input files
-        // (leaving them in place - see the 'noop' flag)
-        // then performs content based routing on the message using XPath
-        from("file:apachecon2012/data?noop=true")
+        BindyCsvDataFormat df = new BindyCsvDataFormat("org.apache.con2012.karafee.integration.model");
+
+        from("file://data/conference/?move=backup/${date:now:yyyyMMdd}/${file:name.noext}.bak")
+          .unmarshal(df)
+          .to("direct:saveConference");
+
+        from("direct:saveConference")
+          .bean(mybean);
+
+        from("timer://apacheCon2012?fixedRate=true&period=25000")
+          .setBody().simple(">> Hello for ApacheCon 2012 conferences ....")
+          .bean(mybean);
+
+        /*
+                from("file:apachecon2012/data?noop=true")
                 .choice()
                 .when(xpath("/person/city = 'Florennes'"))
                 .to("file:target/messages/belgium")
                 .otherwise()
                 .to("file:target/messages/others");
+         */
 
-        from("timer://apacheCon2012?fixedRate=true&period=20000")
-          .setBody().simple(">> Hello for ApacheCon 2012 conferences ....")
-          .bean(mybean);
     }
 
 }
